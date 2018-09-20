33d73ce92e0ffcbca5906a95d493234b1e68a6f8
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-20 18:26:54 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, hashes|
     hashes.each do |attribute2, pigeons|
       pigeons.each do |pigeon|
-        binding.pry
+        #binding.pry
         if new_hash[pigeon][attribute] == nil 
           new_hash[pigeon] = {attribute => [attribute2.to_s]}
         else 
